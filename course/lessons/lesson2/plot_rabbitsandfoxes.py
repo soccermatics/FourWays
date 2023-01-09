d656a6fc90cf6e2ae69a3868d8e01d1e16fa93fd
soccermatics <djtsumpter@me.com>
2023-01-09 18:58:36 +0100

    small rabbits

--- course/lessons/lesson2/plot_rabbitsandfoxes.py
+++ course/lessons/lesson2/plot_rabbitsandfoxes.py
@@ -75,22 +75,26 @@ We aren't going to solve these equations yet. First we need to have the equation
 reaction form these are,
 
 .. math::
+
       \mbox{F} +  \mbox{R} \\xrightarrow{b}  2 \mbox{F} 
 
 for foxes eating rabbits and
 
 .. math::
+
       \mbox{F}   \\xrightarrow{d}  \mbox{D} 
    
 for foxes dying. Converted to differential equations, the rate of change for rabbits becomes
 
 .. math::
+   :label: rabbits
  
    \\frac{dR}{dt} = \\underbrace{a R}_{\mbox{R} \\xrightarrow{a} 2 \mbox{R}} - \\underbrace{b R F}_{\mbox{F} + \mbox{R} \\xrightarrow{b} 2 \mbox{F}}
 
 Similarly, we can write the rate of change of foxes as 
 
 .. math::
+   :label: foxes
  
    \\frac{dF}{dt} =  \\underbrace{c R F}_{\mbox{F} + \mbox{R} \\xrightarrow{c} 2 \mbox{F}} - \\underbrace{d R}_{\mbox{R} \\xrightarrow{d} 2 \mbox{R}}
 
@@ -109,17 +113,165 @@ population grows and the more foxes there are the faster the rabbits are eaten. 
 understand this abstract problem first, before we make any claims about what happens to real 
 rabbits and real foxes. 
 
+
+
+
+Simulating the model
+--------------------
+
+In this section we use Python to run a numerical simulation of the model.
+
 """
 
+# Import the libraries we use
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+from pylab import rcParams
+matplotlib.font_manager.FontProperties(family='Helvetica',size=11)
+rcParams['figure.figsize'] = 9/2.54, 6/2.54
+from scipy import integrate
 
 
 ##############################################################################
-# Simulating the model
-# --------------------
-# Let's do ..
+# We start by defining the model. This code creates a function 
+# which we can use to simulate differential equations :eq:`rabbits` and :eq:`foxes`.
 
-import numpy as np
+# Differential equation
+def dXdt(X, t=0):
+    # Growth rate of fox and rabbit populations.
+    return np.array([ a*X[0]        - b*X[0]*X[1] ,     #Rabbits X[0] is R
+                      c*X[0]*X[1]   - d*X[1]])    #Foxes X[1] is F
+
+
+##############################################################################
+# Next we define the parameter values. You can change these to see how
+# changes to the paramaters leads to changes in the outcome of the model. 
+
+# Parameter values
+a = 5
+b = 1
+c = 0.15
+d = 1
+
+##############################################################################
+# Now we solve the equations numerically
+
+t = np.linspace(0, 20,  1000)               # time
+X0 = np.array([10, 2])                     # initially 10 rabbits and 2 foxes
+X = integrate.odeint(dXdt, X0, t)
+R, F = X.T
+
+fig,ax=plt.subplots(num=1)
+ax.plot(t, R, '-',color='k', label='Rabbits (R)')
+ax.plot(t, F  , '--',color='k', label='Foxes (F)')
+ax.legend(loc='best')
+ax.set_xlabel('Time: t')
+ax.set_ylabel('Population')
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.set_xticks(np.arange(0,12,step=1))
+ax.set_yticks(np.arange(0,50,step=10))
+ax.set_xlim(0,12)
+ax.set_ylim(0,25) 
+plt.show()
+
+##############################################################################
+# First the rabbit populations grow, because there are only two foxes.
+# But this leads to an increase in foxes. Once the population of foxes is sufficiently
+# large, they then start reducing rabbit populations and they die out. Then,
+# when there are few rabbits left, the foxes start to die out too, allowing the rabbit 
+# population to grow again.
+#
+# Visualising the cycle
+# ---------------------
+#
+# In the figure above, we show how foxes and rabbits change over time.
+# We can also plot how they change relative to each other (a so called phase plane). 
+# For the numerical simulations we do this as follows:
 
 
 
-x=np.zeros(1)
+
+def plotPhasePlane(ax,R,F):
+    ax.plot(R, F, '-',color='k', label='Rabbits (R)')
+    ax.set_xlabel('Time: t')
+    ax.set_ylabel('Population')
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.set_xticks(np.arange(0,30,step=5))
+    ax.set_yticks(np.arange(0,20,step=5))
+    ax.set_ylim(0,12)   
+    ax.set_xlim(0,30) 
+    
+    
+fig,ax=plt.subplots(num=1)
+plotPhasePlane(ax,R,F)
+plt.show()
+
+##############################################################################
+# In order to better understand this cycle, in the book, 
+# Parker first looked at 
+# the equilibria where the rate at which rabbits are born equals the rate 
+# at which they die. 
+# 
+# BOOK TEXT HERE
+#
+# We can find the rabbit equilibtirum by solving
+# 
+# .. math::
+# 
+#   \\frac{dR}{dt} = a R - b R F =0 
+#
+# i.e. the number of rabbits does not change over time. This occurs either when 
+# :math:`R=0` (all the rabbits are dead) or when :math:`F=a/b` (when the number of
+# foxes is equal to the birth rate of rabbits divided by the rate at which 
+# foxes eat rabbits).
+#
+# Similarly, we can find the fox equilibtirum by solving
+# 
+# .. math::
+# 
+#   \\frac{dF}{dt} = c R F - d F =0 
+#
+# i.e. the number of foxes does not change over time. This occurs either when 
+# :math:`F=0` (all the foxes are dead) or when :math:`R=d/c` (when the number of
+# rabbits is equal to the death rate of foxes divided by the rate at which 
+# foxes grow after eating rabbits).
+#
+# We can now plot these equilibrium on the phase plane
+# 
+
+fig,ax=plt.subplots(num=1)
+ax.plot([-100,100],[a/b,a/b],linestyle=':',color='k')
+ax.plot([d/c,d/c],[-100,100],linestyle=':',color='k')
+plotPhasePlane(ax,R,F)
+
+##############################################################################
+# The final thing Parker did was draw arrows to indicate the direction 
+# of change. We do this below by evaluating :math:`dR/dt` and :math:`dF/dt`
+# for different values and plotting them.
+
+x = np.linspace(1, 30 ,6)
+y = np.linspace(1, 12, 5)
+X , Y  = np.meshgrid(x, y)
+dX, dY = dXdt([X, Y]) 
+#Make in to unit vectors. 
+M = np.hypot(dX,dY)
+dX = dX/M
+dY = dY/M
+
+fig,ax=plt.subplots(num=1)
+ax.quiver(X, Y, dX, dY, pivot='mid')
+ax.plot([-100,100],[a/b,a/b],linestyle=':',color='k')
+ax.plot([d/c,d/c],[-100,100],linestyle=':',color='k')
+plotPhasePlane(ax,R,F)
+
+
+
+
+
+
+
+
+
