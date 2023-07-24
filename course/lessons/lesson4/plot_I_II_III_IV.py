0180cd1f179b8b7531695686e441833c7b28c9de
soccermatics <djtsumpter@me.com>
2023-07-24 07:25:06 +0200

    IIIIIIIV

--- course/lessons/lesson4/plot_I_II_III_IV.py
+++ course/lessons/lesson4/plot_I_II_III_IV.py
@@ -1,14 +1,88 @@
 """
 I, II, III, IV
 ==============
+
+Throughout the book, we have used one dimensional cellular automata to illustrate ideas. 
+In this section we look at the final class of cellular automata (CA), complex, and investigate 
+some of their properties.
+
+In Santa Fe
+-----------
+
+Here Chris presents a new set of rules,
+
+.. image:: ../../images/lesson4/ChrisToldUs.png
+
+The output of this looks as follows,
+
+.. image:: ../../images/lesson4/Rule 110.png
+
+We can simulate this cellular automata using the same code as we used in the section
+:ref:`cellular chaos<cellularchaos>`, as follows,
+
 """
 
 import numpy as np
 
-##############################################################################
-# This is a title
-# ---------------
-# Let's do ..
+def generate_ca(rule, steps, input_string='', size=50,print_output=1):
+    # Initialize the list of cells with a single "on" cell in the middle
+    if input_string=='':
+        cells = [0] * size
+        cells[size // 2] = 1
+    else:
+        cells=list(map(int, [*input_string]))
+        size=len(input_string)
+    
+    #Store the middle column
+    middle_column=[cells[size // 2]]
+    
+    
+    # Create a dictionary for mapping inputs to outputs 
+    patterns = {
+        (1, 1, 1): rule[0],
+        (1, 1, 0): rule[1],
+        (1, 0, 1): rule[2],
+        (1, 0, 0): rule[3],
+        (0, 1, 1): rule[4],
+        (0, 1, 0): rule[5],
+        (0, 0, 1): rule[6],
+        (0, 0, 0): rule[7]
+    }
+    
+    # Generate the specified number of steps
+    for i in range(steps):
+        # Copy the current state of the cells
+        new_cells = [0] * size
+        
+        # Loop over every cell and update its state based on the neighboring cells
+        for j in range(size):
+            # Get the pattern of the cell and its neighbors
+            if j == 0:
+                pattern = (cells[size-1], cells[0], cells[1])
+            elif j == size-1:
+                pattern = (cells[size-2], cells[size-1], cells[0])
+            else:
+                pattern = (cells[j-1], cells[j], cells[j+1])
+            
+            # Update the cell's state based on the pattern
+            new_cells[j] = patterns[pattern]
+        
+        string=''.join([str(item) for item in cells])
+        if print_output:
+            print(string)
+        
+        # Update the cells with the new state
+        cells = new_cells
+    
+#Set up the rule
+rule = [1, 0, 1, 1, 0, 0, 1, 0] 
+size = 70
+steps = 70
 
-x=np.zeros(2)
-print(x)
+generate_ca(rule, steps,size=size,print_output=1)
+
+##############################################################################
+# Four classes
+# ------------
+
+
